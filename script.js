1398d84a72d492fd28c1324361c847dccfb56e21
klebermkardel <molinakardel@outlook.com.br>
2025-09-19 07:27:22 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+
+Um programa para gerenciar uma pequena coleção de livros, permitindo adicionar, listar e controlar o empréstimo.
+
+Estrutura de Dados: Um array de objetos, onde cada objeto é um livro com propriedades como titulo, autor e disponivel (booleano true/false).
+
+Funcionalidades:
+
+Um menu principal para o usuário navegar.
+
+Uma função adicionarLivro().
+
+Uma função listarLivros() (que deve mostrar se o livro está disponível ou emprestado).
+
+Uma função emprestarLivro() (muda disponivel para false).
+
+Uma função devolverLivro() (muda disponivel para true).
+
+*/
+
